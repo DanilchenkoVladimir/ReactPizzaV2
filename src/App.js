3ece39b214354c8b75a923a23d556ef02e466169
danila <ppgpilot@yandex.ru>
2022-08-13 01:53:15 +0300

    добавлена логика кнопки (useState)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './PizzaBlock';
+import './scss/app.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>hi</h1>
-      </header>
+      <div className="wrapper">
+        <Header />
+          <div className="content">
+            <div className="container">
+              <div className="content__top">
+                <Categories />
+                <Sort />
+              </div>
+                <h2 className="content__title">Все пиццы</h2>
+              <div className="content__items">
+                <PizzaBlock title='мексиканская' price={500} /> 
+                <PizzaBlock title='паприка' price={350} />
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
